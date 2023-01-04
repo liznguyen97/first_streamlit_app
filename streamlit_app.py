133c825e9aa25608ffb72bebb2ab1074a29cca18
Beth <92049269+liznguyen97@users.noreply.github.com>
2023-01-04 11:37:51 -0500

    Update streamlit_app.py
    make the fruityvice data look more user friendly

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())  # just writes the data to the screen
+
+#take the json version of th response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#taking the normalized version and output it onto screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
+
